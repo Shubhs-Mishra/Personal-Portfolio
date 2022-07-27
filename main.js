d23114345566a857bfad1114ba911b11b5ab8d04
Shubham Mishra <91754103+Shubhs-Mishra@users.noreply.github.com>
2022-07-27 14:58:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelector(".chessPlayer").addEventListener("click",function name(params) {
+    document.querySelector("#coder").style.display = "none";
+    document.querySelector("#chess").style.display = "block";
+   });
+// for coder
+document.querySelector(".coderBtn").addEventListener("click",function name(params) {
+    document.querySelector("#coder").style.display = "block";
+    document.querySelector("#chess").style.display = "none";
+});
